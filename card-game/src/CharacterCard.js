fa8c944ef96f89622e1fc5a8e5120d2eb1fc5df0
“nueng040517” <mntogetter@gmail.com>
2019-09-23 20:02:53 +0700

    Commit : N0 8

--- card-game/src/CharacterCard.js
+++ card-game/src/CharacterCard.js
@@ -13,7 +13,12 @@ class CharacterCard extends React.Component {
         if(this.state.active === 0)       
         this.props.activationHandler(this.props.value);
     }
- 
+    componentDidUpdate = (prevProps ) =>{
+        console.log (this.props.attemp);
+        if(prevProps.attemp !== this.props.attemp || prevProps.check !== this.props.check){
+            this.setState({active:0})
+        }
+    }
     render() {
         let activeClass = this.state.active ? 'activeCard' : '';
         let className = `card ${activeClass}`
